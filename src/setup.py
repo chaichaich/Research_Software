de6d2057742c6a0940a75c4eaba63f91e1cd2b22
Samuel  Chai <samuelchai@Samuels-MacBook-Air-2.local>
2024-04-23 21:41:44 -0400

    update

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+
+setup(name='i10pack',
+      version='0.0.1',
+      description='i10 index',
+      maintainer='Samuel Chai',
+      maintainer_email='sechai@andrew.cmu.edu',
+      license='MIT',
+      packages=['i10pack'],
+      entry_points={'console_scripts': ['oa = i10pack.main:main']},
+      long_description='''\
+i10 index
+==============
+Handy functions for a project.''')
